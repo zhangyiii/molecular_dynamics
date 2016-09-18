cfe623f50e80f0cc7db7b7d4338982caa6442b6b
Andrey Parfenov <a1994ndrey@gmail.com>
2016-09-18 14:33:06 +0300

    add time measurement for MC

--- MC/host/src/main.cpp
+++ MC/host/src/main.cpp
@@ -7,8 +7,8 @@
 #define rc 3
 #define box_size 6
 #define N 32
-#define nmax 8000
-#define total_it 16000
+#define nmax 20000
+#define total_it 40000
 #define T 1.3
 #define initial_dist_by_one_axis 1.2
 
@@ -27,6 +27,7 @@ cl_mem output_buf;
 // Problem data(positions and energy)
 cl_float3 input_a[N] = {};
 float output[N * N] = {};
+double total_kernel_time = 0.;
 
 // Function prototypes
 bool init_opencl();
@@ -35,25 +36,23 @@ void run();
 void cleanup();
 void mc();
 float calculate_energy_lj();
-float max_deviation = 0;
+float max_deviation = 0.005;
+
 // Entry point.
 int main() {
-
-    //I dont want to recompile the application with different values
-    printf("enter max_deviation : ");
-    scanf("%f",&max_deviation);
-
+    const double start_time = getCurrentTimestamp();
     // Initialize OpenCL.
     if(!init_opencl()) {
       return -1;
     }
-
     // Initialize the problem data.
     init_problem();
-
     mc();
     // Free the resources allocated
     cleanup();
+    const double end_time = getCurrentTimestamp();
+    printf("\nTime: %0.3f ms\n", (end_time - start_time) * 1e3);
+    printf("Kernel total time : %0.3f ms\n", double(total_kernel_time) * 1e-6);
 
   return 0;
 }
@@ -106,12 +105,12 @@ bool init_opencl() {
     checkError(status, "Failed to create kernel");
 
     // Input buffer.
-    input_a_buf = clCreateBuffer(context, CL_MEM_READ_ONLY, 
+    input_a_buf = clCreateBuffer(context, CL_MEM_READ_ONLY,
         N * sizeof(cl_float3), NULL, &status);
     checkError(status, "Failed to create buffer for input A");
 
     // Output buffer.
-    output_buf = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
+    output_buf = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
         N * N * sizeof(float), NULL, &status);
     checkError(status, "Failed to create buffer for output");
 
@@ -125,7 +124,7 @@ void init_problem() {
         for (double j = 1.1; j < box_size - 1.1; j += initial_dist_by_one_axis) {
             for (double l = 1.1; l < box_size - 1.1 ; l += initial_dist_by_one_axis) {
                 if( count == N){
-                    return; //it is not balanced grid but we can use it 
+                    return; //it is not balanced grid but we can use it
                 }
                 input_a[count] = (cl_float3){ i, j, l };
                 count++;
@@ -145,7 +144,7 @@ float calculate_energy_lj() {
     for (unsigned i = 0; i < N * N; i++)
         total_energy+=output[i];
     total_energy/=2;
-    return total_energy;   
+    return total_energy;
 }
 
 void mc() {
@@ -156,7 +155,13 @@ void mc() {
     float u1 = calculate_energy_lj();
     while (1) {
         if ((i % 100 == 0) && (i != 0)) {
-            printf("good_iter_hung is %d and energy is %f\n", good_iter_hung, energy_ar[good_iter-1]/N);
+            if ( good_iter_hung > 55 ){
+                max_deviation  /=2;
+            }
+            if (good_iter_hung < 45 )
+            {
+                max_deviation *= 2;
+            }
             good_iter_hung = 0;
         }
         if ((good_iter == nmax) || (i == total_it)) {
@@ -173,7 +178,7 @@ void mc() {
             input_a[particle].x = input_a[particle].x + ex;
             input_a[particle].y = input_a[particle].y + ex;
             input_a[particle].z = input_a[particle].z + ex;
-        } 
+        }
         double u2 = calculate_energy_lj();
         double deltaU_div_T = (u1 - u2) / T;
         double probability = exp(deltaU_div_T);
@@ -230,6 +235,9 @@ void run() {
     // Wait for all devices to finish.
     clWaitForEvents(1, &finish_event);
 
+    cl_ulong time_ns = getStartEndTime(kernel_event);
+    total_kernel_time += time_ns;
+
     // Release all events.
     clReleaseEvent(kernel_event);
     clReleaseEvent(finish_event);
@@ -256,4 +264,3 @@ void cleanup() {
     clReleaseContext(context);
     }
 }
-
